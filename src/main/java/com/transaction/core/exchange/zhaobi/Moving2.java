6e4b3b7ff5b34dd158ec9f81d4b8891925c54064
yehanlong <952292896@qq.com>
2019-08-09 11:43:31 +0800

    moving2

--- src/main/java/com/transaction/core/exchange/zhaobi/Moving2.java
+++ src/main/java/com/transaction/core/exchange/zhaobi/Moving2.java
@@ -12,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.concurrent.locks.Lock;
 
 public class Moving2 extends Thread {
@@ -75,13 +77,17 @@ public class Moving2 extends Thread {
                 TradeVO sy2Market = client.getMarketInfo(sy2+"USDT");
                 double yccPrice = sy2Market.getSells().get(0).getPrice();
                 double yccNum = sy2Market.getSells().get(0).getAm();
-                if(yccPrice*yccNum < 2.0 || !sy2Market.getSuccess()){
+                BigDecimal yccPriceB = new BigDecimal(yccPrice);
+                BigDecimal yccNumB = new BigDecimal(yccNum);
+                int a = (yccPriceB.multiply(yccNumB)).compareTo(new BigDecimal(2.0));
+                if(a == -1 || !sy2Market.getSuccess()){
                     // 当获取失败或者金额太少，就放弃此次循环
                     Thread.sleep(5000);
                     continue;
                 }
-                double yccCount = usdt/yccPrice;
-
+                //double yccCount = usdt/yccPrice;
+                BigDecimal usdtB = new BigDecimal(usdt);
+                BigDecimal yccCountB = usdtB.divide(yccPriceB,25, RoundingMode.HALF_DOWN);
 
                 // 获取bty价格
                 TradeVO sy1Market = client.getMarketInfo(sy1+"USDT");
@@ -90,28 +96,38 @@ public class Moving2 extends Thread {
                 TradeVO sy12Market = client.getMarketInfo(sy2+sy1);
                 double ybPrice = sy12Market.getBuys().get(0).getPrice();
                 double ybNum = sy12Market.getBuys().get(0).getAm();
+                BigDecimal ybPriceB = new BigDecimal(ybPrice);
+                BigDecimal ybNumB = new BigDecimal(ybNum);
+                int a1 = (ybPriceB.multiply(ybNumB).multiply(new BigDecimal(sy1Market.getBuys().get(0).getPrice())))
+                        .compareTo(new BigDecimal(2.0));
                 // bty数量*bty价格
-                if (ybPrice*ybNum*sy1Market.getBuys().get(0).getPrice() < 2.0 || !sy12Market.getSuccess()) {
+                if (a1 == -1 || !sy12Market.getSuccess()) {
                     Thread.sleep(5000);
                     continue;
                 }
                 // 最终获得的bty
-                double btyCount = yccCount*ybPrice;
+                //double btyCount = yccCount*ybPrice;
+                BigDecimal btyCountB = yccCountB.multiply(ybPriceB);
 
                 // 卖掉BTY
 
                 double btyPrice = sy1Market.getBuys().get(0).getPrice();
                 double btyNum = sy1Market.getBuys().get(0).getAm();
+                BigDecimal btyPriceB = new BigDecimal(btyPrice);
+                BigDecimal btyNumB = new BigDecimal(btyNum);
                 // 最终获得的usdt
-                double usdtcount = btyPrice*btyCount;
+                //double usdtcount = btyPrice*btyCount;
+                BigDecimal usdtcountB = btyPriceB.multiply(btyCountB);
+                int a2 = (btyPriceB.multiply(btyNumB)).compareTo(new BigDecimal(2.0));
                 if(btyPrice*btyNum < 2.0 || !sy1Market.getSuccess()){
                     Thread.sleep(5000);
                     continue;
                 }
-                logger.debug("预计一轮usdt：",usdtcount);
+                logger.debug("预计一轮usdt：",usdtcountB.doubleValue());
 
                 // 判断一轮交易后的去掉手续费（3次=3*0.001），是否有盈利
-                if (usdtcount > usdt*(1+0.0035)) {
+                int a3 = usdtcountB.compareTo(usdtB.multiply(((new BigDecimal(1)).add(new BigDecimal(0.0035)))));
+                if (a3 == 1) {
 
                     // 有盈利，开始交易
 
@@ -129,26 +145,34 @@ public class Moving2 extends Thread {
                     TradeVO sy2Market1 = client.getMarketInfo(sy2+"USDT");
                     double yccPrice1 = sy2Market1.getSells().get(0).getPrice();
                     double yccNum1 = sy2Market1.getSells().get(0).getAm();
-                    if(yccPrice1*yccNum1 < 2.0 || !sy2Market1.getSuccess()){
+                    BigDecimal yccPrice1B = new BigDecimal(yccPrice1);
+                    BigDecimal yccNum1B = new BigDecimal(yccNum1);
+                    int a4 = (yccPrice1B.multiply(yccNum1B)).compareTo(new BigDecimal(2.0));
+                    if(a4 == -1 || !sy2Market1.getSuccess()){
                         // 当获取失败或者金额太少，就放弃此次循环
                         Thread.sleep(5000);
                         logger.warn("yccPrice1*yccNum1 < 2.0 !sy2Market1.getSuccess 该订单部分被别人吃掉");
                         continue;
                     }
-                    if (yccPrice != yccPrice1){
+                    int a5 = yccPriceB.compareTo(yccPrice1B);
+                    if (a5 != 0){
                         // 价格发生变化，立即进行下次循环
                         logger.warn("yccPrice != yccPrice1 该订单已经被别人吃掉");
                         continue;
                     }
-                    double yccCount1 = everyUSDT*(1-0.001)/yccPrice1;
-                    if(yccNum1<yccCount1){
-                        yccCount1 = yccNum1;
+                    //double yccCount1 = everyUSDT*(1-0.001)/yccPrice1;
+                    BigDecimal everyUSDTB = new BigDecimal(everyUSDT);
+                    BigDecimal yccCount1B = everyUSDTB.multiply((new BigDecimal(1).subtract(new BigDecimal(0.001))))
+                            .divide(yccPrice1B,25,RoundingMode.HALF_DOWN);
+                    int a6 = yccNum1B.compareTo(yccCount1B);
+                    if(a6 == -1){
+                        yccCount1B = yccNum1B;
                     }
                     // todo
 //                    if (btyPrice1*btyNum1 > 2.0 && btyPrice1*btyNum1 < 4.0){
 //                        btyCount1 = btyNum1;
 //                    }
-                    boolean success1 = client.postBill(yccCount1,sy2,"USDT",yccPrice1,"buy");
+                    boolean success1 = client.postBill(yccCount1B.doubleValue(),sy2,"USDT",yccPrice1,"buy");
                     if (!success1){
                         logger.error("第一次交易挂单失败");
                         continue;
@@ -158,30 +182,38 @@ public class Moving2 extends Thread {
                     TradeVO sy12Market1 = client.getMarketInfo(sy2+sy1);
                     double ybPrice1 = sy12Market1.getBuys().get(0).getPrice();
                     double ybNum1 = sy12Market1.getBuys().get(0).getAm();
+                    BigDecimal ybPrice1B = new BigDecimal(ybPrice1);
+                    BigDecimal ybNum1B = new BigDecimal(ybNum1);
                     if (!sy12Market1.getSuccess()) {
                         Thread.sleep(5000);
                         continue;
                     }
                     // 发现价格变化还是继续执行，亏也就亏一次
-                    if (ybPrice1*ybNum1*btyPrice < 2.0) {
+                    int a7 = (ybPrice1B.multiply(ybNum1B).multiply(btyPriceB)).compareTo(new BigDecimal(2.0));
+                    if (a7 == -1) {
                         logger.warn("ybPrice1*ybNum1 < 2.0 该订单部分被别人吃掉");
                     }
-                    if (ybPrice != ybPrice1) {
+                    int a8 = ybPriceB.compareTo(ybPrice1B);
+                    if (a8 != 0) {
                         logger.warn("ybPrice != ybPrice1 该订单已经被别人吃掉");
                         //continue;
                     }
 
                     double accYCC = client.getAccount(sy2).getActive();
-                    if (accYCC < yccCount1) {
+                    BigDecimal accYCCB = new BigDecimal(accYCC);
+                    int a9 = accYCCB.compareTo(yccCount1B);
+                    if (a9 == -1) {
                         logger.warn("accYCC < yccCount1 账户余额不足");
-                        yccCount1 = accYCC;
+                        yccCount1B = accYCCB;
                     }
-                    double btyCount1 = yccCount1*(1-0.001)*ybPrice1;
-                    if(ybNum1 < yccCount1){
+                    //double btyCount1 = yccCount1*(1-0.001)*ybPrice1;
+                    BigDecimal btyCount1B = yccCount1B.multiply((new BigDecimal(1)).subtract(new BigDecimal(0.001))).multiply(ybPrice1B);
+                    int a10 = ybNum1B.compareTo(yccCount1B);
+                    if(a10 == -1){
                         logger.warn("ybNum1 < yccCount1 该订单部分被别人吃掉");
-                        yccCount1 = ybNum1;
+                        yccCount1B = ybNum1B;
                     }
-                    Boolean success2 = client.postBill(yccCount1,sy2,sy1,ybPrice1,"sell");
+                    Boolean success2 = client.postBill(yccCount1B.doubleValue(),sy2,sy1,ybPrice1,"sell");
                     if (!success2){
                         logger.error("第二次交易挂单失败");
                         continue;
@@ -192,24 +224,30 @@ public class Moving2 extends Thread {
                     double btyPrice1 = sy1Market1.getBuys().get(0).getPrice();
                     double btyNum1 = sy1Market1.getBuys().get(0).getAm();
 //                    double usdtcount1 = yccCount*yccPrice;
+                    BigDecimal btyPrice1B = new BigDecimal(btyPrice1);
+                    BigDecimal btyNum1B = new BigDecimal(btyNum1);
                     if(!sy1Market1.getSuccess()){
                         Thread.sleep(5000);
                         continue;
                     }
-                    if(btyPrice1*btyNum1 < 2.0 ) {
+                    int a11 = (btyPrice1B.multiply(btyNum1B)).compareTo(new BigDecimal(2.0));
+                    if(a11 == -1) {
                         logger.warn("btyPrice1*btyNum1 < 2.0 该订单部分被别人吃掉");
                     }
-                    if(btyPrice1 != btyPrice) {
+                    int a12 = btyPrice1B.compareTo(btyPriceB);
+                    if(a12 != 0) {
                         logger.warn("yccPrice1 != yccPrice 该订单已经被别人吃掉");
 //                        continue;
                     }
 
                     double accBTY = client.getAccount(sy1).getActive();
-                    if (accBTY < btyCount1)  {
+                    BigDecimal accBTYB = new BigDecimal(accBTY);
+                    int a13 = accBTYB.compareTo(btyCount1B);
+                    if (a13 == -1)  {
                         logger.warn("accBTY < btyCount1");
-                        btyCount1 = accBTY;
+                        btyCount1B = accBTYB;
                     }
-                    Boolean success3 = client.postBill(btyCount1,sy1,"USDT",yccPrice1,"sell");
+                    Boolean success3 = client.postBill(btyCount1B.doubleValue(),sy1,"USDT",yccPrice1,"sell");
                     if (!success3){
                         logger.error("第三次交易挂单失败");
                         break;
