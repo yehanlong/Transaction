cc1c5e18dde9ae1833b29949937729e500e2d592
吴少杰 <1536161955@qq.com>
2019-08-21 20:57:16 +0800

    11

--- src/main/java/com/transaction/core/exchange/pubinterface/Exchange.java
+++ src/main/java/com/transaction/core/exchange/pubinterface/Exchange.java
@@ -31,6 +31,8 @@ public interface Exchange {
     boolean syncPostBill(String symbol1, String symbol2, double amount1, double amount2,double amount3,
                          double price1, double price2,double price3, String type);
 
-    public String getName();
+    String getName();
+
+
 
 }
