08ab2b238608a0fc6b8e697d84a60913ab937bac
kfm主机 <1376822651@qq.com>
2019-08-25 00:17:52 +0800

    修改缓存时间的bug

--- src/main/java/com/transaction/core/strategy/Moving.java
+++ src/main/java/com/transaction/core/strategy/Moving.java
@@ -7,12 +7,16 @@ import java.util.concurrent.locks.Lock;
 public class Moving implements Strategy{
     @Override
     public void start(Lock lock, Exchange client, String sy1, String sy2, String sBase) {
+        new Thread(()->{
+            SyncDo doBuy = new SyncDo(client,sy1,sy2,sBase,lock,"BUY");
+            doBuy.setSType(2);
+            doBuy.doIt();
+        },sBase+"_"+sy1+"_"+sy2+"_BUY/MOVING").start();
+        new Thread(()->{
+            SyncDo doSell = new SyncDo(client,sy1,sy2,sBase,lock,"Sell");
+            doSell.setSType(2);
+            doSell.doIt();
+        },sBase+"_"+sy1+"_"+sy2+"_SELL/MOVING").start();
 
-        SyncDo doBuy = new SyncDo(client,sy1,sy2,sBase,lock,"BUY");
-        doBuy.setSType(2);
-        doBuy.doIt();
-        SyncDo doSell = new SyncDo(client,sy1,sy2,sBase,lock,"Sell");
-        doSell.setSType(2);
-        doSell.doIt();
     }
 }
--- src/main/java/com/transaction/core/strategy/SyncMoving.java
+++ src/main/java/com/transaction/core/strategy/SyncMoving.java
@@ -10,12 +10,16 @@ import java.util.concurrent.locks.Lock;
 public class SyncMoving implements Strategy {
     @Override
     public void start(Lock lock, Exchange client, String sy1, String sy2, String sBase) {
+        new Thread(()->{
+            SyncDo syncDoBuy = new SyncDo(client,sy1,sy2,sBase,lock,"BUY");
+            syncDoBuy.setSType(3);
+            syncDoBuy.doIt();
+        },sBase+"_"+sy1+"_"+sy2+"_BUY/SYNCMOVING").start();
+        new Thread(()->{
+            SyncDo syncDoSell = new SyncDo(client,sy1,sy2,sBase,lock,"Sell");
+            syncDoSell.setSType(3);
+            syncDoSell.doIt();
+        },sBase+"_"+sy1+"_"+sy2+"_SELL/SYNCMOVING").start();
 
-        SyncDo syncDoBuy = new SyncDo(client,sy1,sy2,sBase,lock,"BUY");
-        syncDoBuy.setSType(3);
-        syncDoBuy.doIt();
-        SyncDo syncDoSell = new SyncDo(client,sy1,sy2,sBase,lock,"Sell");
-        syncDoSell.setSType(3);
-        syncDoSell.doIt();
     }
 }
